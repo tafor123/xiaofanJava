ff14b9edf93fe50f736cbcc564cc9fa8ea0c7960
小凡 <3276139871@qq.com>
2023-08-29 20:25:14 +0800

    毕设

---
+++ 小白做毕设/vue/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    主页
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
